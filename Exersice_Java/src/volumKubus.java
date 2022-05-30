744830a8c9022105de3269e64e2328c93258e571
Mystereyn <teukureynaldi106@gmail.com>
2022-05-30 22:05:34 +0700

    Modified volumKubus.java

--- Exersice_Java/src/volumKubus.java
+++ Exersice_Java/src/volumKubus.java
@@ -1,17 +1,18 @@
-import java.Utl.Scanner;
+import java.util.Scanner;
 public class volumKubus {
     public static void main(String[] args) {
         
-        //N adalah inputan untuk mengisi variable kubus
-        int  sisi1 , sisi2 , sisi3;
-        int hasil = sisi1*sisi2*sisi3;
+        //hasil adalah inputan untuk mengisi variable kubus
+        int sisi1;
+        int sisi2;
+        int sisi3;
 
-        Scanner input = new Scanner(System.in) // salah disini 
+        Scanner input = new Scanner (System.in); // salah disini 
         System.out.println("=====Volum kubus by Mystereyn====");
         System.out.println();
 
         System.out.print("Masukkan angka Variable sisi1 : ");
-        sisi1  = input.nextInt();
+        sisi1 =  input.nextInt();
 
         System.out.print("Masukkan angka Variable sisi2 : ");
         sisi2 = input.nextInt();
@@ -19,9 +20,7 @@ public class volumKubus {
         System.out.print("Masukkan angka variable sisi3 : ");
         sisi3 = input.nextInt();
         
-        System.out.println("Hasil keseluruhan = " + hasil);
+        System.out.println("Hasil keseluruhan = " +  (sisi1*sisi2*sisi3));
         
-        input.closed(); // untuk menutup terminal secara otomatis
-
     }
 }
