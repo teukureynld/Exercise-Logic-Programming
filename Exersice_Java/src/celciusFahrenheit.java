1daf95e607c104be097c7c6feb5dcffe1981858e
Mystereyn <teukureynaldi106@gmail.com>
2022-05-31 14:23:13 +0700

    Add celciusFahrenheit.java

---
+++ Exersice_Java/src/celciusFahrenheit.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class celciusFahrenheit {
+    public static void main(String[] args) {
+        
+        // float untuk membagi , hasilnya akan menjadi pecahan.
+        // memakai switch 
+        double Fahrenheit , Celcius;
+
+        Scanner input = new Scanner (System.in);
+
+        System.out.print("Input Fahrenheit= " );
+        Fahrenheit = input.nextDouble();
+
+        System.out.print("Input Celcius = ");
+        Celcius = input.nextDouble();
+
+        
+
+
+    }    
+}
