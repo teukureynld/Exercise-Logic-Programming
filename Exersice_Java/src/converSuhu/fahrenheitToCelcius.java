43545f65e9c6ae872cf0a423ac26207a2a403aa0
Mystereyn <teukureynaldi106@gmail.com>
2022-06-01 11:54:44 +0700

    Add new folder Suhu dan anak ayam

---
+++ Exersice_Java/src/converSuhu/fahrenheitToCelcius.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class fahrenheitToCelcius {
+
+    public static void main(String[] args) {
+        
+        
+        double f , c; 
+
+        Scanner input = new Scanner (System.in);
+
+        System.out.println("=== By Mystereyn ===");
+        System.out.println();
+        
+        System.out.print("Input angka untuk di convert : " );
+        f = input.nextFloat();
+
+        c = ((f-32) *5 / 9);  
+        System.out.println("Hasil dari fahrenheit ke celcius adalah " + c);
+
+
+        input.close();
+    }
+       
+}
