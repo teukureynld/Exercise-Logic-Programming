8e12cabf61ab316764cd542d48b3ef8a73635ff5
Mystereyn <teukureynaldi106@gmail.com>
2022-06-03 06:56:47 +0700

    Add folder MenuUtama within its file.

---
+++ Exersice_Java/src/menuUtama/MenuUtama.java
@@ -0,0 +1,30 @@
+package menuUtama;
+import java.util.Scanner;
+
+public class MenuUtama {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner (System.in);
+
+        int pilihMenu;
+
+        System.out.print("Masukkan pilihan Menu : ");
+        pilihMenu = input.nextInt();
+
+        System.out.println("=====Pilihan Menu=====");
+        switch(pilihMenu) {
+            case 1 -> System.out.println("1. Baca Data");
+            case 2 -> System.out.println("2. Cetak Data");
+            case 3 -> System.out.println("3. Ubah Data");
+            case 4 -> System.out.println("4. Hapus Data");
+
+            default -> {
+                System.out.println("Salah input!");
+            
+            }   
+        }
+        input.close();
+    }
+    
+}
