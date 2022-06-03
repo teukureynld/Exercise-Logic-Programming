dc7034d800adcef3cc3c1a6acbeb0809318aea7f
Mystereyn <teukureynaldi106@gmail.com>
2022-06-03 18:41:44 +0700

    modified MenuUtama.java

--- Exersice_Java/src/menuUtama/MenuUtama.java
+++ Exersice_Java/src/menuUtama/MenuUtama.java
@@ -7,26 +7,28 @@ public class MenuUtama {
         
         Scanner input = new Scanner (System.in);
 
-        int pilihMenu;
+        int pilihMenu = 0;
 
-        System.out.print("Masukkan pilihan Menu : ");
-        pilihMenu = input.nextInt();
+        while ( pilihMenu != 6 ) {
+                System.out.print("Masukkan pilihan Menu : ");
+                pilihMenu = input.nextInt();
 
-        System.out.println("=====Pilihan Menu=====");
+                System.out.println("=====Pilihan Menu=====");
 
-        //menggunakan switch lambda
-        switch(pilihMenu) {
-            case 1 -> System.out.println("1. Baca Data");
-            case 2 -> System.out.println("2. Cetak Data");
-            case 3 -> System.out.println("3. Ubah Data");
-            case 4 -> System.out.println("4. Hapus Data");
+                //menggunakan switch lambda
+                switch(pilihMenu) {
+                case 1 -> System.out.println("1. Baca Data");
+                case 2 -> System.out.println("2. Cetak Data");
+                case 3 -> System.out.println("3. Ubah Data");
+                case 4 -> System.out.println("4. Hapus Data");
+                case 5 -> System.out.println("5. Exit.....");
 
-            default -> {
-                System.out.println("Salah input!");
+                default -> {
+                    System.out.println("Salah input!");
+                input.close();
+                }
+            }
             
-            }   
         }
-        input.close();
     }
-    
 }
