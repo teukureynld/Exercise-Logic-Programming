a23e4e65d796ed973c0de46ac085cd24b5c6cd91
Mystereyn <teukureynaldi106@gmail.com>
2022-10-03 19:07:14 +0700

    Belajar Bintang Pyramid Java, tapi polanya masih mirip uler

---
+++ Exersice_Java/src/Bintang_Pyramid/BintangPyramid.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+public class BintangPyramid {
+    public static void main(String[] args) {
+        // untuk input jumlah bintang, kedua variable ini adalah tipe variable yang
+        // global
+        int n;
+        Scanner input = new Scanner (System.in);
+
+        System.out.print("Masukkan jumlah pola yang diinginkan : ");
+        n = input.nextInt();
+        
+        System.out.print(" ");
+
+        // untuk perulangan n (baris) inputan
+        for(int rows = 1; rows <= n; rows++ ) {
+            // ini perulangan untuk spasi baris dan kolom
+            for(int coloumn = 1; coloumn <= n-rows; coloumn++) {
+                System.out.print(" ");
+            }
+            // cetak bintang
+            for(int star = 1; star <= rows; star++ ) {
+                System.out.println("*");
+            }
+        }
+        input.close();
+    }
+}
