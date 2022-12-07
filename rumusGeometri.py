6b1f94fc50700a2eaab4d6824ef07645da39da59
Mystereyn <teukureynaldi106@gmail.com>
2022-12-07 20:57:05 +0700

    Uploading IDEA JetBrains Component and deleting Python-Branch instead

---
+++ rumusGeometri.py
@@ -0,0 +1,22 @@
+def luasSegitiga() :
+    a  = int(input("Input nilai variable a : "))
+    t = int(input("Input nilai variable t : "))
+    l = 1/2 * a * t
+    
+    print(f'hasilnya adalah {l}');
+
+# def resultLargeOfTriangel() :
+luasSegitiga()
+
+# resultLargeOfTriangel()
+
+print("\n");
+
+def luasPersegiPanjang ():
+    p = int(input("Input nilai variable p : "))
+    l = int(input("Input nilai l (lebar) : "))
+    L = p * l 
+
+    print(f'Hasil luas Persegi Panjang adalah {L}');
+
+luasPersegiPanjang();
